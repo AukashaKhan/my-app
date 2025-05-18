05c26d798fdb7aa0ebf5f7af66d14bfbd4cb9b9d
Aukasha <aukashakhan30@gmail.com>
2025-05-18 08:14:16 +0500

    test

--- src/App.js
+++ src/App.js
@@ -299,7 +299,7 @@ const [isAdding, setIsAdding] = useState(false);
   return (
     <div style={styles.container}>
       <nav style={styles.sidebar}>
-        <h2 style={styles.sidebarTitle}>My React App</h2>
+        <h2 style={styles.sidebarTitle}>My App</h2>
         <ul style={styles.navList}>
           {["users", "posts", "todos"].map((p) => (
             <li
